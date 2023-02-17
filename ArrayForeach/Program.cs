fa54f5fbefab70e207a84faff31ac4c5f83c514a
Sharifbek97 <sharifbekmirzozoda2@gmail.com>
2023-02-17 19:48:01 +0500

    Added array foreach

---
+++ ArrayForeach/Program.cs
@@ -0,0 +1,13 @@
+﻿var students = new []{"Ali","Vali","Gani"};
+foreach (var student in students)
+{
+    Console.WriteLine(student);    
+}
+
+//project of second
+
+var numbers = new []{1,3,4,0,-12};
+foreach (var number in numbers)
+{
+    Console.WriteLine(number);    
+}
